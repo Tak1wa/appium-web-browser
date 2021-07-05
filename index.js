b41f9577ecb2dcadff624c2f8ec062d66a53b146
iwasatakahito <iwasa.takahito@classmethod.jp>
2021-07-06 02:47:19 +0900

    Fixed for browser testing with Device Farm.

--- index.js
+++ index.js
@@ -5,26 +5,38 @@ const opts = {
   path: '/wd/hub',
   port: 4723,
   capabilities: {
-    platformName: "Android",
-    platformVersion: "11",
-    deviceName: "Android Emulator",
-    //app: "/Users/iwasa.takahito/Downloads/ApiDemos-debug.apk",
-    //appPackage: "io.appium.android.apis",
-    //appActivity: ".view.TextFields",
-    browserName: "Chrome",
-    automationName: "UiAutomator2"
+    // platformName: "Android",
+    // platformVersion: "9",
+    // deviceName: "Android Emulator",
+    // browserName: "Chrome",
+    // automationName: "UiAutomator2"
   }
 };
 
+const DEFAULT_TIMEOUT = 15000;
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 async function main () {
   const client = await wdio.remote(opts);
 
-  Feature
-  client.get();
-  const field = await client.$("android.widget.EditText");
-  await field.setValue("Hello World!");
-  const value = await field.getText();
-  assert.strictEqual(value,"Hello World!");
+  await client.url("https://classmethod.jp/");
+
+  const menuButton = await client.$(".header__menu");
+  await menuButton.waitForDisplayed({timeout: DEFAULT_TIMEOUT});
+  await client.saveScreenshot(process.env.SCREENSHOT_PATH + "/1.png");
+
+  await menuButton.click();
+
+  await sleep(2000);
+  await client.touchScroll(0, 500);
+  await sleep(1000);
+
+  const siryoButton = await client.$('a[href="/download/company-overview/"]');
+  await siryoButton.waitForDisplayed({timeout: DEFAULT_TIMEOUT});
+  await client.saveScreenshot(process.env.SCREENSHOT_PATH + "/2.png");
+
+  await siryoButton.click();
+  await sleep(5000);
+  await client.saveScreenshot(process.env.SCREENSHOT_PATH + "/3.png");
 
   await client.deleteSession();
 }
